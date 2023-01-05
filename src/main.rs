43ed3c5c7fbfa0f1a35fa5577dab7de56c5a9017
ripe-mangos <lincoln@lncn.dev>
2023-01-05 17:03:20 +0000

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use lastpass_blob_deobfuscator::Results;
+use std::{fs::File, io::Read};
+
+fn main() {
+    let fpath = std::env::args().skip(1).next().unwrap();
+
+    let mut f = File::open(fpath).unwrap();
+    let mut buf = String::new();
+    f.read_to_string(&mut buf).unwrap();
+
+    let res = Results::try_from(buf.as_str()).unwrap();
+    println!("Account version: {}\nAccount CBC: {}", res.version, if res.acct_cbc { "enabled" } else { "disabled" });
+
+    let ecb_accts = res.accounts.iter().filter(|x| !x.login.cbc);
+    if ecb_accts.clone().count() > 0 {
+        println!("AES-ECB accounts found!");
+        for a in ecb_accts {
+            println!(" - {}", a.url);
+        }
+    } else {
+        println!("No AES-ECB accounts found. :)");
+    }
+}
