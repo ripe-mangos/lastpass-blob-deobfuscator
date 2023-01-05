8684fab5b79eeb7d76db079c9032c73de9a1fdf7
ripe-mangos <lincoln@lncn.dev>
2023-01-05 17:07:11 +0000

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use lastpass_blob_deobfuscator::Results;
+use lpass_blob::Results;
 use std::{fs::File, io::Read};
 
 fn main() {
@@ -7,10 +7,10 @@ fn main() {
     let mut f = File::open(fpath).unwrap();
     let mut buf = String::new();
     f.read_to_string(&mut buf).unwrap();
-
+    
     let res = Results::try_from(buf.as_str()).unwrap();
     println!("Account version: {}\nAccount CBC: {}", res.version, if res.acct_cbc { "enabled" } else { "disabled" });
-
+    
     let ecb_accts = res.accounts.iter().filter(|x| !x.login.cbc);
     if ecb_accts.clone().count() > 0 {
         println!("AES-ECB accounts found!");
